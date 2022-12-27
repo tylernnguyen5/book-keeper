bf34ac644e71dd6b6411e3bb2e45e4729b534b09
Tyler Nguyen <tyler.nnguyen5@gmail.com>
2022-12-27 17:23:52 +1100

    Finalized book-keeper app

---
+++ README.md
@@ -0,0 +1,15 @@
+HOW IT WORKS
+
+- this is an application for storing your favorite website bookmarks
+- it has local storage saving feature, which means you once you save the bookmark(s), you can turn off the browser, come back and all the bookmark(s) will remain
+- if the local storage is empty, the application with populate it with 1 object data
+- you can also delete unwanted bookmark(s)
+- the bookmark contains an website name and an url
+- the favicon of the website is populate dynamically
+
+WHAT I LEARN
+
+- CRUD for Local Storage
+- Dynamic populating favicon
+- Regular Expression to validate URl
+- Object de-constructing
---
+++ script.js
@@ -0,0 +1,136 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+
+let bookmarks = [];
+let bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show modal, Focus on Input
+function showModal() {
+  modal.classList.add('show-modal');
+  websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => console.log(e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Valudate Form
+function validate(nameValue, urlValue) {
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/ig;
+  const regex = new RegExp(expression);
+
+  if (!nameValue || !urlValue) {
+    alert('Please submit values for both fields.');
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+  }
+  // Valid
+  return true;
+}
+
+// Fetch Bookmarks
+function fetchBookmarks () {
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    bookmarks = [
+      {
+        name: 'test',
+        url: 'https://test.com'
+      }
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
+// Build Bookmarks DOM
+function buildBookmarks() {
+  // Remove all bookmark elements
+  bookmarksContainer.textContent = '';
+
+  bookmarks.forEach((bookmark) => {
+    const {name, url} = bookmark; // de-constructing
+    
+    // Item
+    const item = document.createElement('div');
+    item.classList.add('item');
+
+    // Close Icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+
+    // Favicon / Link Container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+      // Favicon
+      const favicon = document.createElement('img');
+      favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+      favicon.setAttribute('alt', 'Favicon');
+
+      // Link
+      const link = document.createElement('a');
+      link.setAttribute('href', `${url}`);
+      link.setAttribute('target', '_blank');
+      link.textContent = name;
+
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  })
+}
+
+// Handle Data from Form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('http://', 'https://')) {urlValue = `https://${urlValue}`}
+
+  console.log(nameValue, urlValue);
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue
+  };
+
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
+} 
+
+// Delete Bookmark
+function deleteBookmark(url) {
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+  });
+
+  // Update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+}
+
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
+
+// on Load, Fetch Bookmarks
+fetchBookmarks();
